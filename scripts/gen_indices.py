cdc36619558c996190dc65121da6de957f1831e7
zhuto@omenpro2 <zhuto@omenpro2.com>
2018-10-11 10:53:13 +0800

    标题生成脚本

---
+++ scripts/gen_indices.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+current_path = os.path.abspath(__file__)
+root_path = os.path.dirname(os.path.dirname(current_path))
+target = os.path.join(root_path, 'README.md')
+headline_pat = re.compile(r'#+ \d')
+
+def fprint(base, level, fmt='{0}- [{1}]({2}{1})'):
+    print(fmt.format((level-1)*'  ', base, level*'#'))
+
+print(target)
+with open(target, 'r', encoding='utf8') as fp:
+    for line in fp:
+        match_obj = headline_pat.match(line)
+        if not match_obj:
+            continue
+        title = line.strip('#').strip()
+        level = len(match_obj.group())-2
+        fprint(title, level)
+        
+
+
+
